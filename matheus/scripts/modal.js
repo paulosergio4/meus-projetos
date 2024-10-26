39cd7ccc1d7951ba475b1c802aea45522a6b4324
PAULO SERGIO <110654467+paulosergio4@users.noreply.github.com>
2024-10-26 19:56:40 -0300

    fiz atualização no mapa e cabeçalho

--- matheus/scripts/modal.js
+++ matheus/scripts/modal.js
@@ -1,7 +1,7 @@
 // Imagens de cada categoria
 const images = {
     empresarial: ["fotos/empresarial/ft11.webp", "fotos/empresarial/empresarial1.webp", "fotos/empresarial/empresarial2.webp", "fotos/empresarial/empresarial3.webp"],
-    gestante: ["fotos/gestante/ft15.jpg", "fotos/gestante/ft3.jpg", "fotos/gestante/ft12.jpg", "fotos/gestante/ft15.jpg", "fotos/gestante/ft16.jpg",  "fotos/gestante/ft17.jpg"],
+    gestante: ["fotos/gestante/ft15.webp", "fotos/gestante/ft3.webp", "fotos/gestante/ft12.webp", "fotos/gestante/ft16.jpg",  "fotos/gestante/ft17.jpg"],
     feminino: ["fotos/feminino/fem1.webp", "fotos/feminino/fem2.webp","fotos/feminino/fem3.webp","fotos/feminino/fem4.webp","fotos/feminino/fem5.webp","fotos/feminino/fem6.webp"],
     casal: ["fotos/casal/ft4.webp", "fotos/casal/ft5.webp", "fotos/casal/casal3.webp", "fotos/casal/casal4.webp"]
 };
